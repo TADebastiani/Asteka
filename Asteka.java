29c41e7ec3134a93e0044339626ca060f87cd5f0
Tiago Debastiani <debastianister@gmail.com>
2014-09-24 22:04:18 -0300

    acrescentado cabeçalho

--- Asteka.java
+++ Asteka.java
@@ -1,20 +1,32 @@
+/**
+* Interpretador da linguagem 'Ateka'.
+*
+* Trabalho realizado para a matéria de Programação I do curso 
+* Ciencia da Computação - UFFS.
+* 
+* Por:
+* Carine Viecile <carine.viecile@gmail.com>
+* Tiago A. Debastiani <debastianister@gmail.com>
+*/
+
 import java.util.Scanner;
-import java.util.regex.Pattern;
+import java.io.*;
 
 class Asteka {
 
 	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
+		File f = new File(args[0]);
+		Scanner sc = new Scanner(f);
 		String linha[] = new String[2000];
-		
 		int i=0;
-		while (100){
-			linha[i] = sc.next();
+		
+		while (sc.hasNext()){
+			linha[i] = sc.nextLine();
 			i++;
 		}
-				
-		for (i=0; linha[i] != null; i++)
+					
+		for (i=0; linha[i] != null; i++){
 			System.out.println(i + ": " + linha[i] + ".");
-
+		}
 	}
 }
