2b379ff890b9613d724029fa8662197401385aed
Tiago Debastiani <debastianister@gmail.com>
2014-09-28 23:50:34 -0300

    Corrigido metodo de procura

--- Variaveis.java
+++ Variaveis.java
@@ -2,29 +2,37 @@ class Variaveis{
 
 	private String variaveis[] = new String[2000];
 	private String valores[] = new String[2000];
-	protected int index; // possiveis classes filha não podem 
+	protected int index; // possiveis classes filha não podem alterar
 
-	public String getVariavel(int index){
-		return this.valores[index];
+	// retorna o valor da variável no indice x
+	public String getVariavel(int x){
+		return this.valores[x];
 	}
 
+	// guarda o nome da variavel e seu valor em dois vetores no mesmo indice
 	public void setVariavel(String variavel, String valor){
 		this.variaveis[index] = variavel;
 		this.valores[index] = valor;
 		this.index++;
 	}
 
-	public int procuraVariavel(String variavel){
-		/** 
-		*	pode ser substituido por <variaveis.indexOf()>
-		*/
-		for (i=0; variaveis[index] != null; index++) {
-			if (variaveis[index] == variavel){
-				index;
-			}else{
-				return null; // em teste
-			}
+	// imprime o nome da variavel e seu valor
+	public void imprimeVariavel(int index){
+		System.out.println("Variavel: " + this.variaveis[index]);
+		System.out.println("Valor: " + this.valores[index]);
+	}
 
+	// procura no vetor de variaveis se existe a variavel recebida como parametro
+	public int procuraVariavel(String variavel){
+		//	não pode ser substituido por <variaveis.indexOf()>
+		int ind = -1;
+		variavel = variavel.trim();
+		for (int i=0; variaveis[i] != null; i++) {
+			if (variaveis[i].equals(variavel)){
+				ind = i;
+			}
 		}
+
+		return ind;
 	}
 }
