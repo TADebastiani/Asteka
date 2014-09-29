320d1c603f5bb7fb832c7f523a6f26ef712beacd
Tiago Debastiani <debastianister@gmail.com>
2014-09-28 23:51:17 -0300

    Metodo de erros de compilação

---
+++ Erro.java
@@ -0,0 +1,18 @@
+class Erro{
+	
+	public Erro(int e, int linha){
+		System.out.println("Na linha: " + (linha+1));
+	
+
+		switch(e){
+			case 1:
+				System.out.println("\tSem ':' no final da linha!");
+				break;
+			case 2:
+				System.out.println("\tVariável não declarada!");
+				break;
+		}
+	
+		System.exit(0);
+	}
+}
