ea19a2815a970d2282e560d6169a06ec76fea7b8
Tiago Debastiani <debastianister@gmail.com>
2014-10-01 21:06:11 -0300

    Adicionado método imprime

--- Funcoes.java
+++ Funcoes.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 
 class Funcoes{
 	
-	Erro erro;
+	private String auxVar, auxVal;
+	private Scanner sc;
 
 
 	public void decodLinha(String linha[], Variaveis var){
-		String auxVar, auxVal;
-		Scanner sc;
 		int x;
 
 		for (int i=0; linha[i] != null; i++){		
 			sc = new Scanner(linha[i]);
 
-			if (sc.hasNext("var")){
+			if (linha[i].startsWith("var")){
 				sc.next();
 				auxVar = sc.next().replaceAll(":",""); 
 
 				// System.out.println(auxVar);
 				
-				if (sc.hasNext("=")){
-					auxVal = linha[i].substring(linha[i].indexOf("=")+2, linha[i].indexOf(":"));
+				if (sc.hasNext("|")){
+					auxVal = linha[i].substring(linha[i].indexOf("|")+2, linha[i].indexOf(":"));
 					// System.out.println(auxVal);
 					var.setVariavel(auxVar, auxVal);
 				}else {
@@ -44,12 +43,39 @@ class Funcoes{
 					if (x > -1){ 
 						System.out.println("imprimindo: " + var.getVariavel(x));
 					} else{
-						erro = new Erro(2, i);
+						Erro.erro(2, i);
 					}
 				}
+
+				// imprime(linha[i], i);
 			}
 
 		}
 	}
+
+	// AINDA EM TESTE
+
+	/* public void imprime(String linha, int i){
+		 String aux;
+		 int x;
+		// verifica se é uma frase ou uma variável
+		if (linha.contains("'")){ // se tiver ' é uma string
+
+			// alterar para imprimir sem enter
+			System.out.printf("imprimindo: " + linha.substring(linha.indexOf("'")+1, linha.indexOf("':")));
+
+		}else { // se não, é uma variavel
+			
+			aux = linha.substring(linha.indexOf("e ")+2, linha.indexOf(":"));
+			x = Variaveis.procuraVariavel(auxVar);
+			
+			// verifica se a variavel existe, transformar em um metodo em Variaveis
+			if (x > -1){ 
+				System.out.println("imprimindo: " + Variaveis.getVariavel(x));
+			} else{
+				Erro.erro(2, i);
+			}
+		}
+	} */
 
 }
