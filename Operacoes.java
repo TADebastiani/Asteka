cd5705376206f6cba412f67fb4b5541e5fbe553c
user <carineviecili@gmail.com>
2014-10-19 23:03:31 -0200

    Metodo Operacoes Alterado

--- Operacoes.java
+++ Operacoes.java
@@ -1,123 +1,58 @@
-class Operacoes{
+class Operacoes extends Variaveis{
 	
 
-	 public void Operacoes(String linha, String [] guardaVar,double [] guardaValores,int linhasGuardaVar){
+	 static public Double Operacoes(String variavel, String valores){
 
 		String aux = "vazio", antesigual, atribui = "vazio";
-		double res, auxiliar, auxiliar2, auxiliar3;
+		double res, auxiliar, valor;
 		char operador = 0;
 		int cont_atribui = 1, atribu = 0;
 		
-
-		aux = linha.substring(0, linha.indexOf("!"));
-		//System.out.println(aux);
-		//possivel verificação de erro
-
 		//verifica o token e atribui ele ao operador
-		for(int i = 0; i < linha.length(); i++){
-			if(linha.charAt(i) == '+' || linha.charAt(i) == '-' || linha.charAt(i) == '*' || linha.charAt(i) == '/' || linha.charAt(i) == '%'){
-				operador = linha.charAt(i);
-
-			}
-			else if(linha.charAt(i) == '!'){
-				atribui = linha.substring(linha.indexOf("!")+1, linha.indexOf(":"));//string que recebe o que será atribuido
-				atribu = 1;
-			}
-			else if(operador != 0){
-				res = Opera(operador, linha, guardaVar, guardaValores, linhasGuardaVar);
-				valorAtual(res, aux, guardaVar, guardaValores, linhasGuardaVar );
-
-				break;
-
+		for(int i = 0; i < valores.length(); i++){
+			if(variavel.charAt(i) == '+' || variavel.charAt(i) == '-' || variavel.charAt(i) == '*' || variavel.charAt(i) == '/' || variavel.charAt(i) == '%'){
+				operador = variavel.charAt(i);
 			}
 
 		}
 
-		if(cont_atribui == 1 && atribu ==1){
-			double valor_atribuicao = antesToken (atribui, guardaVar, guardaValores, linhasGuardaVar);
+		//repassa as variaveis auxiliares 
+		String aux1 = variavel.substring(variavel.indexOf("!")+1, variavel.indexOf(operador));
+		String aux2 = variavel.substring(variavel.indexOf(operador)+1, variavel.indexOf(":"));
 
-			valorAtual(valor_atribuicao, aux, guardaVar, guardaValores, linhasGuardaVar);
+		//repassa os
+		Double auxiliar2 = Double.parseDouble(aux1);
+		Double auxiliar3 = Double.parseDouble(aux2);
 
-			cont_atribui = 1;
-			atribu = 0;
-		}
-	}
-
-
-		//verifica os valores de antes do token
-		static public double antesToken(String aux1, String [] guardaVar, double [] guardaValores, int linhasGuardaVar){
-			double auxiliar = 0;
-			int i = 0;
-
-			if(aux1.matches("^[0.0-9.0]*$")){
-				auxiliar = Double.parseDouble(aux1);
-			}
-			else{
-				for(i =0; i < linhasGuardaVar; i++){
-					if(guardaVar[i].equals(aux1)){
-						auxiliar = guardaValores[i];
-					}
-				}
-			}
-			return auxiliar;//retorna o valor após o =
-		} 
-
-		//verifica os valores depois do token
-		static public double depoisToken(String aux2, String [] guardaVar, double [] guardaValores, int linhasGuardaVar){
-			double auxiliar = 0;
-			int i = 0;
-
-			if(aux2.matches("^[0.0-9.0]*$")){
-				auxiliar = Double.parseDouble(aux2);
-			}
-			else{
-				for(i =0; i < linhasGuardaVar; i++){
-					if(guardaVar[i].equals(aux2)){
-						auxiliar = guardaValores[i];
-					}
-				}
-			}
-			return auxiliar;//retorna o valor após o =
-		}
-
-		static public void valorAtual (double resultado, String aux, String [] guardaVar, double [] guardaValores, int linhasGuardaVar ){
-			for (int i = 0; i<linhasGuardaVar; i++) {
-				if(guardaVar[i].equals(aux)){
-					guardaValores[i] = resultado;
-					break;
-				}
-				
-			}
-		}
-
-		static public double Opera(char operando, String linha, String [] guardaVar, double [] guardaValores, int linhasGuardaVar){
 			double result = 0;
-			String aux1 = linha.substring(linha.indexOf("!")+1, linha.indexOf(operando));
-			String aux2 = linha.substring(linha.indexOf(operando)+1, linha.indexOf(":"));
-			
-			double auxiliar2 = antesToken(aux1, guardaVar, guardaValores, linhasGuardaVar);
-			double auxiliar3 = depoisToken(aux2, guardaVar, guardaValores, linhasGuardaVar);
 
-			if(operando == '*'){
+			if(operador== '*'){
 				result = auxiliar2 + auxiliar3;
 
-			}else if(operando == '/'){
+			}else if(operador == '/'){
 				result = auxiliar2 - auxiliar3;
 
-			}else if(operando == '+'){
+			}else if(operador == '+'){
 				result = auxiliar2 * auxiliar3;
 
-			}else if(operando == '-'){
+			}else if(operador == '-'){
 				result = auxiliar2 / auxiliar3;
 
-			}else if(operando == '%'){
+			}else if(operador == '%'){
 				result = auxiliar2 % auxiliar3;
 
-			}else if(operando == '!'){
+			}else if(operador == '!'){
 				result = auxiliar2;
 		        }
 		return result;
+		
 		}
-	}	
+}
+
+		
+
+		
+		
+		
 	
 
