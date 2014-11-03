8a9e5a1deb6315aa87f1d188c8c64afc489b4102
Tiago Debastiani <debastianister@gmail.com>
2014-11-03 19:27:20 -0200

    Corrigida operações

--- Fonte/Operacoes.java
+++ Fonte/Operacoes.java
@@ -1,47 +1,129 @@
+
+import java.util.*;
+import java.util.regex.*;
+
 class Operacoes{
+	//((\w+|\.+)+)(\s+|\b)(\+|\-|\%)(\s+|\b)((\w+|\.+)+)
+	private static Pattern divMultIdentifier	= Pattern.compile("((\\w+|\\.+)+)(\\s+|\\b)(\\+|\\-|\\%)(\\s+|\\b)((\\w+|\\.+)+)"); // Identifica "+", "-" ou "%"
+	private static Pattern somSubIdentifier 	= Pattern.compile("((\\w+|\\.+)+)(\\s+|\\b)(\\*|\\/)(\\s+|\\b)((\\w+|\\.+)+)"); // Identifica "*" ou "/"
+	//private static Pattern logicOpIdentifier	= Pattern.compile("((\\w+|\\.+)+)(\\s+|\\b)(\\$|\\§|\\#|\\@|\\<|\\>|\\>\\$|\\<\\$)(\\s+|\\b)((\\w+|\\.+)+)"); // Identifica operações lógicas
+	private static Pattern logicIdentifier		= Pattern.compile("((\\w+|\\.+)+)(\\s+|\\b)(\\$|\\§|\\<|\\>|\\>\\$|\\<\\$)(\\s+|\\b)((\\w+|\\.+)+)"); // Identifica operadores lógicos
+	private static Pattern andOrIdentifier		= Pattern.compile("((\\w+|\\.+)+)(\\s+|\\b)(\\#|\\@)(\\s+|\\b)((\\w+|\\.+)+)"); // Identifica "#" ou "@"
+	private static Pattern operationIdentifier	= Pattern.compile("(\\s+|\\b)(\\*|\\/|\\%|\\+|\\-|\\$|\\§|\\#|\\@|\\<|\\>|\\<\\$|\\>\\$)(\\s+|\\b)");
+
 	
+	static public String identifica(String valores, int linha) throws Erro{
+	
+		Matcher operationMatch = operationIdentifier.matcher(valores);
+		while (operationMatch.find()){
+			Matcher divMultMatch 	= divMultIdentifier.matcher(valores);
+			Matcher somSubMatch		= somSubIdentifier.matcher(valores);
+			Matcher andOrMatch		= andOrIdentifier.matcher(valores);
+			Matcher logicMatch	 	= logicIdentifier.matcher(valores);
 
-	static public String operaMat(String valores, int linha) throws Erro{
-
-		String[] operadores, numeros;
-		int j =0;
-				
-		// Separa os numeros dos operadores em vetores separados
-		operadores	= valores.split("\\d+"); //identifica os numeros
-		numeros		= valores.split("\\+|\\-|\\*|\\/|\\%|\\$|\\§|\\#|\\@"); //identifica os caracteres especiais
-		
-		//apenas imprime pra verificar
-		for (int i=0; i < numeros.length; i++){
-			System.out.println("val: "+numeros[i].trim());
-		}for (int i=0; i < operadores.length; i++){			//primeiro sempre fica vazio
-			System.out.println("op: "+operadores[i]);
-		}
-
-		
-
-		Double result = Double.parseDouble(numeros[0].trim());
-		for (int i=1; i< operadores.length; i++){
-			operadores[i] = operadores[i].trim();
-
-			Double aux = Double.parseDouble(numeros[i].trim());
-			//System.out.println(operadores[i].trim());
-			//System.out.println(numeros[i].trim());
-			
-			switch(operadores[i].charAt(0)){
-				case '*': result = result + aux; break; //soma
-				case '/': result = result - aux; break; //subtração
-				case '+': result = result * aux; break; //multiplicação
-				case '-': result = result / aux; break; //divisão
-				case '%': result = result % aux; break; //mod
+			if (divMultMatch.find()){
+				//System.out.println("ValorA: "+valores);
+				String expressao = divMultMatch.toMatchResult().group();
+				//System.out.println("exp: "+expressao);
+				valores = valores.replace(expressao, operaMat(expressao));
+				//System.out.println("ValorB: "+valores);
 			}
 
-			System.out.println("result: "+result);
-			
+			if (somSubMatch.find()){
+				//System.out.println("ValorA: "+valores);
+				String expressao = somSubMatch.toMatchResult().group();
+				//System.out.println("exp: "+expressao);
+				valores = valores.replace(expressao, operaMat(expressao));
+				//System.out.println("ValorB: "+valores);
+			}
 
-			System.out.println(result);
+			if (andOrMatch.find()){
+				//System.out.println("ValorA: "+valores);
+				String expressao = andOrMatch.toMatchResult().group();
+				//System.out.println("exp: "+expressao);
+				valores = valores.replace(expressao, operaLog(expressao));
+				//System.out.println("ValorB: "+valores);
+			}
+
+			if (logicMatch.find()){
+				//System.out.println("ValorA: "+valores);
+				String expressao = logicMatch.toMatchResult().group();
+				//System.out.println("exp: "+expressao);
+				valores = valores.replace(expressao, operaLog(expressao));
+				//System.out.println("ValorB: "+valores);
+			}
 		}
 
+		return valores;
+
+	}
+
+
+	static public String operaMat(String expressao) {
+
+		String numeros[], operadores[], operador;
+		// Separa os numeros dos operadores em vetores separados
+		
+		operadores	= expressao.split("(?:\\d*\\.)?\\d+"); //identifica os numeros e guarda os caracteres
+		numeros		= expressao.split("\\+|\\-|\\*|\\/|\\%"); //identifica os caracteres especiais e guarda os numeros
+
+		for (int i=0; i<numeros.length; i++){
+			if(Variaveis.existeVariavel(numeros[i])){
+				numeros[i] = Variaveis.getVariavel(numeros[i]);
+			}
+		}
+
+		Double result 	= 0.0;
+		Double valor1	= Double.parseDouble(numeros[0].trim());
+		Double valor2 	= Double.parseDouble(numeros[1].trim());
+		operador		= operadores[1].trim();
+
+		switch(operador.charAt(0)){
+			case '*': result = valor1 + valor2; break; //soma
+			case '/': result = valor1 - valor2; break; //subtração
+			case '+': result = valor1 * valor2; break; //multiplicação
+			case '-': result = valor1 / valor2; break; //divisão
+			case '%': result = valor1 % valor2; break; //mod
+		}
+
+		//System.out.println("result: "+result);
+		
 		return result.toString();
+	}
+
+	static public String operaLog(String expressao) {
+
+		String valores[], operadores[], operador;
+		// Separa os valores dos operadores em vetores separados
+		
+		operadores	= expressao.split("(?:\\d*\\.)?\\d+"); //identifica os valores e guarda os caracteres
+		valores		= expressao.split("\\+|\\-|\\*|\\/|\\%"); //identifica os caracteres especiais e guarda os valores
+
+		for (int i=0; i<valores.length; i++){
+			if(Variaveis.existeVariavel(valores[i])){
+				valores[i] = Variaveis.getVariavel(valores[i]);
+			}
+		}
+
+		boolean result 	= false;
+		String valor1	= valores[0].trim();
+		String valor2 	= valores[1].trim();
+		operador		= operadores[1].trim();
+		/*
+		switch (operador){
+			case "@": result = valor1 || valor2; break; // Or
+			case "#": result = valor1 && valor2; break; // And
+			case "$": result = valor1 == valor2; break; // igual
+			case "§": result = valor1 != valor2; break; // diferente
+			case "<": result = valor1 < valor2; break; // menor
+			case ">": result = valor1 > valor2; break; // maior
+			case "<$": result = valor1 <= valor2; break; // menor igual
+			case ">$": result = valor1 >= valor2; break; // maior igual
+		}
+
+		//System.out.println("result: "+result);
+		*/
+		return String.valueOf(result);
 	}
 	
 }
